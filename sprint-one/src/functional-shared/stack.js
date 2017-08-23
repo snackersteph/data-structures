e08d864db089aed3235bd814aa5b4fc47e96f55c
Hack Reactor Student <student@hackreactor.com>
2017-08-23 15:58:07 -0700

    complete functional shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -14,19 +14,60 @@ var Stack = function() {
 };
 
 
-
-
 var stackMethods = {};
 
 stackMethods.push = function(value) {
   this.count++;
   this.storage[this.count] = value;
-}
+};
 
+stackMethods.pop = function() {
+  var output = this.storage[this.count];
+  delete this.storage[this.count];
+  this.count--;
+  return output;
+};
 
 stackMethods.size = function() {
+  if (this.count < 0) {
+    this.count = 0;
+  }
   return this.count;
-}
+};
+
+
+
+
+
+// var Stack = function() {
+//   var someInstance = {};
+
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+//   var count = 0;
+//   // Implement the methods below
+//   someInstance.push = function(value) {
+//     count ++;
+//     storage[count] = value;
+//   };
+
+//   someInstance.pop = function() {
+//     var output = storage[count];
+//     delete storage[count];
+//     count --;
+//     return output;
+//   };
+
+//   someInstance.size = function() {
+//     if (count < 0) {
+//       count = 0;
+//     }
+//     return count;
+//   };
+
+//   return someInstance;
+// };
+
 
 
 
