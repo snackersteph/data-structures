199519a4592a2c47b53ad25f6c595a596638ed6f
Hack Reactor Student <student@hackreactor.com>
2017-08-23 15:28:50 -0700

    pass two functional shared tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = {};
+  
+  obj.storage = {};
+  obj.count = 0;
+
+  obj.push = stackMethods.push;
+  obj.pop = stackMethods.pop;
+  obj.size = stackMethods.size;
+
+
+
+  return obj;  
 };
 
+
+
+
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+}
+
+
+stackMethods.size = function() {
+  return this.count;
+}
+
 
 
