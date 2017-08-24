1a552802cb467dfe38a1884ac82082c1aebd4ea7
Hack Reactor Student <student@hackreactor.com>
2017-08-24 13:01:27 -0700

    Complete tail and remove head

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.head.next = newNode;
+    }
+    this.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (!this.head) { return null; }
+    // Save the head value to a variable to return
+    var val = this.head.value;  //4
+    var garbage = this.head;    
+  
+    // Assign the next node the head property
+    if (this.head) {
+      
+    // Declare the new head
+      this.head = this.head.next;
+       console.log('head',this.head)
+    }
+    // Delete the previous head
+    delete garbage;
+    // Return value of deleted head
+    return val;
   };
 
   list.contains = function(target) {
+    // if (this.value === target)
   };
 
   return list;
@@ -27,3 +51,11 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var ll = LinkedList();
+ll.addToTail(4);
+console.log(ll);
+
+
+
+
