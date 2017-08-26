be7a63f0cac84be015fb9ce6c12585ca4ab83f1f
Stephanie Wong <swongwrites@gmail.com>
2017-08-25 17:43:29 -0700

    graph progress

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -8,6 +8,7 @@ var Graph = function() {
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   this.storage[node] = {};
+  this.storage[node].edges = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -17,25 +18,44 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var pointerDelete = this.storage[node].edges //3
+  // var edgesArray = this.storage[fromNode].edges
+  // for (i=0; i < edgesArray.length; i++) {
+  //   if (this.storage[fromNode].edges )
+  //  //iterate the edgesArray
+  // } 
+
+  // if (this.storage[i].edge === pointerDelete) {
+  //   this.storage[i].edge = null;
+  // } ;
+
   delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  return this.storage[fromNode].edge === toNode;
+  var edgesArray = this.storage[fromNode].edges
+  if (edgesArray.indexOf(toNode)) {
+    return true;
+  }
+  var otherEdgesArray = this.storage[toNode].edges
+  if (edgesArray.indexOf(fromNode)) {
+    return true;
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.storage[fromNode].edge = toNode;
-  this.storage[toNode].edge = fromNode;
+  this.storage[fromNode].edges.push(toNode);
+  this.storage[toNode].edges.push(toNode)
   
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  this.storage[fromNode].edge = null;
-  this.storage[toNode].edge = null;
+  this.storage[fromNode].edges = null;
+  this.storage[toNode].edges = null;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
@@ -51,6 +71,7 @@ graph.addNode(2);
 graph.addNode(1);
 graph.addNode(3);
 graph.addEdge(3, 2);
+graph.removeNode(3);
 
 console.log(graph.storage)
 
