a196a6547a7e9436a1daa0e6c3a1302b86850979
Stephanie Wong <swongwrites@gmail.com>
2017-08-26 15:59:47 -0700

    Complete binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,76 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(littleTree)
+  obj.value = value;
+  obj.right = null;
+  obj.left = null;
+
+  return obj;
 };
+
+var littleTree = {}
+
+littleTree.insert = function (num) {
+  var newNode = BinarySearchTree(num);
+
+  // check if value is bigger
+  // if bigger, value is root.right
+  // check if value is smaller
+  // if smaller, value is root.left
+
+  if (this.value < num) {
+    if (this.right === null) {
+      this.right = newNode;
+    } else {
+      this.right.insert(num);
+    } 
+  } else {
+    if (this.left === null) {
+      this.left = newNode;
+    } else {
+      this.left.insert(num);
+    }
+  }
+}
+
+littleTree.contains = function (num) {
+  if (this.value === num) {
+    return true;
+  }
+
+  if (this.value < num && this.right !== null) {
+    return this.right.contains(num);
+  } else {
+    return false;
+  } 
+  
+  if (this.value > num && this.left !== null) {
+    return this.left.contains(num);
+  } else {
+    return false;
+  }
+}
+
+littleTree.depthFirstLog = function (cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+}
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var array = [];
+var func = function(value) { array.push(value); };
+
+var plant = BinarySearchTree(5);
+plant.insert(2);
+plant.insert(3);
+console.log(plant);
+console.log(plant.depthFirstLog(func));
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,10 @@
-
+/*
+  calculate an index
+  retrieve a bucket at that location
+  iterate over the bucket
+    perform an action if the key is found
+    otherwise perform a not-found action
+*/
 
 var HashTable = function() {
   this._limit = 8;
@@ -11,10 +17,20 @@ HashTable.prototype.insert = function(k, v) {
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+
+  for (var i=0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
 };
 
 
